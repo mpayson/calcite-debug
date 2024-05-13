17dffbe4727ef4687032c2c696dd60350a48a4e8
maxp8108 <mpayson@esri.com>
2024-05-13 12:25:35 -0400

    Combobox repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { setAssetPath } from '@esri/calcite-components/dist/components';
+import { setAssetPath } from "@esri/calcite-components/dist/components";
 import "@esri/calcite-components/dist/calcite/calcite.css";
-import "@esri/calcite-components/dist/components/calcite-tabs";
-import "@esri/calcite-components/dist/components/calcite-tab-nav";
-import "@esri/calcite-components/dist/components/calcite-tab-title";
-import "@esri/calcite-components/dist/components/calcite-tab";
-import "@esri/calcite-components/dist/components/calcite-notice";
+import "@esri/calcite-components/dist/components/calcite-combobox";
+import "@esri/calcite-components/dist/components/calcite-combobox-item";
 import {
-  CalciteTabs,
-  CalciteTabNav,
-  CalciteTabTitle,
-  CalciteTab,
-  CalciteNotice,
+  CalciteCombobox,
+  CalciteComboboxItem,
 } from "@esri/calcite-components-react";
 
 setAssetPath(location.href);
 
+function App() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && (
+        <CalciteCombobox>
+          <CalciteComboboxItem value="1" textLabel="One" />
+          <CalciteComboboxItem value="2" textLabel="Two" />
+        </CalciteCombobox>
+      )}
+    </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CalciteTabs>
-      <CalciteTabNav slot="title-group">
-        <CalciteTabTitle selected>Watercraft</CalciteTabTitle>
-        <CalciteTabTitle>Automobiles</CalciteTabTitle>
-        <CalciteTabTitle>Aircrafts</CalciteTabTitle>
-      </CalciteTabNav>
-      <CalciteTab selected>
-        <CalciteNotice icon="embark" open>
-          <div slot="message">Recommended for coastal use</div>
-        </CalciteNotice>
-      </CalciteTab>
-      <CalciteTab>
-        <CalciteNotice icon="car" open>
-          <div slot="message">A good choice for inland adventure</div>
-        </CalciteNotice>
-      </CalciteTab>
-      <CalciteTab>
-        <calcite-notice icon="plane" open>
-          <div slot="message">Cross continents quickly</div>
-        </calcite-notice>
-      </CalciteTab>
-    </CalciteTabs>
+    <App />
   </React.StrictMode>
 );
