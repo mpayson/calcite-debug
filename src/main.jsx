6666cba78b9eccba9eb0ee60fd07925976d6aa84
maxp8108 <mpayson@esri.com>
2024-06-03 10:58:53 -0400

    popover

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom/client";
-import { setAssetPath } from '@esri/calcite-components/dist/components';
+import { setAssetPath } from "@esri/calcite-components/dist/components";
 import "@esri/calcite-components/dist/calcite/calcite.css";
-import "@esri/calcite-components/dist/components/calcite-tabs";
-import "@esri/calcite-components/dist/components/calcite-tab-nav";
-import "@esri/calcite-components/dist/components/calcite-tab-title";
-import "@esri/calcite-components/dist/components/calcite-tab";
-import "@esri/calcite-components/dist/components/calcite-notice";
-import {
-  CalciteTabs,
-  CalciteTabNav,
-  CalciteTabTitle,
-  CalciteTab,
-  CalciteNotice,
-} from "@esri/calcite-components-react";
+import "@esri/calcite-components/dist/components/calcite-popover";
+import "@esri/calcite-components/dist/components/calcite-button";
+import { CalciteButton } from "@esri/calcite-components-react";
 
 setAssetPath(location.href);
 
+function App() {
+  function onClick(event) {
+    const popover = document.createElement("calcite-popover");
+    popover.referenceElement = event.target;
+    popover.heading = "Popover";
+    popover.open = true;
+    popover.innerText = `My text`;
+    document.body.appendChild(popover);
+  }
+
+  return (
+    <div>
+      <CalciteButton appearance="solid" color="blue" onClick={onClick}>
+        Click me
+      </CalciteButton>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CalciteTabs>
-      <CalciteTabNav slot="title-group">
-        <CalciteTabTitle selected>Watercraft</CalciteTabTitle>
-        <CalciteTabTitle>Automobiles</CalciteTabTitle>
-        <CalciteTabTitle>Aircrafts</CalciteTabTitle>
-      </CalciteTabNav>
-      <CalciteTab selected>
-        <CalciteNotice icon="embark" open>
-          <div slot="message">Recommended for coastal use</div>
-        </CalciteNotice>
-      </CalciteTab>
-      <CalciteTab>
-        <CalciteNotice icon="car" open>
-          <div slot="message">A good choice for inland adventure</div>
-        </CalciteNotice>
-      </CalciteTab>
-      <CalciteTab>
-        <calcite-notice icon="plane" open>
-          <div slot="message">Cross continents quickly</div>
-        </calcite-notice>
-      </CalciteTab>
-    </CalciteTabs>
+    <App />
   </React.StrictMode>
 );
