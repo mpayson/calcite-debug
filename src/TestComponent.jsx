a8d6686312a14852a7aa64ad7da07a41b5e9aaad
maxp8108 <mpayson@esri.com>
2024-11-22 08:40:38 -0500

    Popover reopen

--- src/TestComponent.jsx
+++ src/TestComponent.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import "@esri/calcite-components/dist/components/calcite-popover";
 import "@esri/calcite-components/dist/components/calcite-button";
-import { Button, Popover } from "@esri/calcite-components-react";
+import { CalciteButton, CalcitePopover } from "@esri/calcite-components-react";
 
 function TestComponent() {
   const [ref, setRef] = useState(null);
   return (
     <>
-      <Button ref={setRef}>Test</Button>
-      <Popover heading="Test" referenceElement={ref} autoClose></Popover>
+      <CalciteButton ref={setRef}>Test</CalciteButton>
+      <CalcitePopover heading="Test" referenceElement={ref} autoClose></CalcitePopover>
     </>
   );
 }
