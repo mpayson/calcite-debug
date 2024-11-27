f1e3c9c1376f8b4fe3cb70b026866aa22a50f71b
maxp8108 <mpayson@esri.com>
2024-11-27 17:23:54 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { setAssetPath } from "@esri/calcite-components";
+import "@esri/calcite-components/dist/components/calcite-button";
 import { CalciteButton } from "@esri/calcite-components-react";
 
 setAssetPath(location.href);
